d6a34b8a1156c5ba8adfeb90266061dbc8db025b
HY <cyn1025@naver.com>
2025-03-08 22:10:34 +0900

    - 중도커밋 (OAuth2로 로그인 구현) (OAuth 로그아웃 기능 추가)

--- src/main/java/me/hyeyul/springbootblog/controller/TokenApiController.java
+++ src/main/java/me/hyeyul/springbootblog/controller/TokenApiController.java
@@ -3,9 +3,11 @@ package me.hyeyul.springbootblog.controller;
 import lombok.RequiredArgsConstructor;
 import me.hyeyul.springbootblog.dto.CreateAccessTokenRequest;
 import me.hyeyul.springbootblog.dto.CreateAccessTokenResponse;
+import me.hyeyul.springbootblog.service.RefreshTokenService;
 import me.hyeyul.springbootblog.service.TokenService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TokenApiController {
     private final TokenService tokenService;
+    private final RefreshTokenService refreshTokenService;
 
     @PostMapping("/api/token")
     public ResponseEntity<CreateAccessTokenResponse> createNewAccessToken
@@ -22,5 +25,13 @@ public class TokenApiController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new CreateAccessTokenResponse(newAccessToken));
+    }
+
+    @DeleteMapping("/api/refresh-token")
+    public ResponseEntity deleteRefreshToken() {
+        refreshTokenService.delete();
+
+        return ResponseEntity.ok()
+                .build();
     }
 }
--- src/main/java/me/hyeyul/springbootblog/service/RefreshTokenService.java
+++ src/main/java/me/hyeyul/springbootblog/service/RefreshTokenService.java
@@ -1,18 +1,30 @@
 package me.hyeyul.springbootblog.service;
 
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import me.hyeyul.springbootblog.config.jwt.TokenProvider;
 import me.hyeyul.springbootblog.domain.RefreshToken;
 import me.hyeyul.springbootblog.repository.RefreshTokenRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 public class RefreshTokenService {
     private final RefreshTokenRepository refreshTokenRepository;
+    private final TokenProvider tokenProvider;
 
     public RefreshToken findByRefreshToken(String refreshToken) {
         return refreshTokenRepository.findByRefreshToken(refreshToken)
                 .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
+
+    @Transactional
+    public void delete(){
+        String token = SecurityContextHolder.getContext().getAuthentication().getCredentials().toString();
+        Long userId = tokenProvider.getUserId(token);
+
+        refreshTokenRepository.deleteByUserId(userId);
     }
 
 }
